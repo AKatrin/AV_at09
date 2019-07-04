4d09528307af137e2d6a59e3d34a02c2f265857b
Areliez Vargas <areliez.vargas@gmail.com>
2019-07-04 10:12:22 -0400

    Update checkstyle

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -5,13 +5,22 @@ import org.apache.commons.collections.list.GrowthList;
  * Person class.
  */
 public class Person {
-    private final String name;
+    /**
+     * Variable for the name person.
+     */
+    private String name;
 
-    public Person(String name) {
+    /**
+     * Constructor of person sending the name.
+     */
+    public Person(final String name) {
         this.name = name;
         new GrowthList();
     }
 
+    /**
+     * Return the name person.
+     */
     public String getName() {
         return name;
     }
